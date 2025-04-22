7685e87dffc01363a6612558db6dc8f5a39a3c28
aishanoor1439 <aisha.noor1439@gmail.com>
2025-04-22 09:45:32 +0500

    Second commit
    completed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,24 @@ namespace Polymorphism
     {
         static void Main(string[] args)
         {
+            //Vehicle v = new Car();
+            //v.startEngine();
+            //Vehicle v1 = new MotorBike();
+            //v1.startEngine();
+            //Vehicle v2 = new Truck();
+            //v2.startEngine();
+            List <Vehicle> vehicles = new List<Vehicle>
+            {
+                new MotorBike(),
+                new Car(),
+                new Truck()
+
+            };
+            foreach(Vehicle v in vehicles)
+            {
+                v.startEngine();
+            }
+
         }
     }
 }
---
+++ Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Vehicle
+    {
+        public virtual void startEngine()
+        {
+            Console.WriteLine("Engine has been started.");
+        }
+
+    }
+    class Truck : Vehicle
+    {
+        public override void startEngine()
+        {
+            Console.WriteLine("Truck engine has been started.");
+        }
+    }
+    class Car : Vehicle
+    {
+        public override void startEngine()
+        {
+            Console.WriteLine("Car engine has been started.");
+        }
+    }
+    class MotorBike : Vehicle
+    {
+        public override void startEngine()
+        {
+            Console.WriteLine("Motor bike engine has been started.");
+        }
+        public new void display()
+        {
+            Console.WriteLine("I am a child class.");
+        }
+    }
+}
